be5e453160782bc5134f73930d94f2d2dbae1919
lmontegu <lmontegu@MacBook-Air-de-Tapi.local>
2024-12-06 00:13:17 -0300

    fix some things

--- components/debt-table.tsx
+++ components/debt-table.tsx
@@ -3,8 +3,9 @@
 import { Debt } from '@prisma/client'
 import { columns } from './debts/columns'
 import { DataTable } from './debts/data-table'
+import { DebtWithClient } from '../utils/debtProcessor'
 
-const DebtTable = ({ debts }: { debts: Debt[] }) => {
+const DebtTable = ({ debts }: { debts: DebtWithClient[] }) => {
   return <DataTable columns={columns} data={debts} />
 }
 
